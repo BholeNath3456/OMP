fbf77c63e621453f1f91a4fcf39a17c67ffeb513
Bhole Nath Maddheshiya <59330174+BholeNath3456@users.noreply.github.com>
2021-03-28 12:27:47 +0530

    Address Uploaded Successfully from Form and check list size

--- OnlineMoneyPay/app/src/main/java/com/example/onlinemoneypay/DBqueries.java
+++ OnlineMoneyPay/app/src/main/java/com/example/onlinemoneypay/DBqueries.java
@@ -36,7 +36,8 @@ public class DBqueries {
     public static List<String> idList = new ArrayList<>();
     public static List<String> myRatedIds = new ArrayList<>();
     public static List<Long> myRating = new ArrayList<>();
-    public static  List<AddressesModel> addressesModelList =new ArrayList<>();
+    public static int selectedAddress = -1;
+    public static List<AddressesModel> addressesModelList = new ArrayList<>();
 
 
     public static void loadCategories(RecyclerView categoryRecyclerView, Context context) {
@@ -285,12 +286,15 @@ public class DBqueries {
                         deliveryIntent = new Intent(context, AddAddressActivity.class);
                     } else {
 
-//                        for(long x=1; x<listSize+1; x++ ){
-//                        addressesModelList.add(new AddressesModel(task.getResult().get("fullname_"+x).toString(),
-//                                task.getResult().get("address_"+x).toString(),
-//                                task.getResult().get("pincode_"+x).toString(),
-//                                (boolean)task.getResult().get("selected_"+x)));
-//                        }
+                        for (long x = 1; x < listSize + 1; x++) {
+                            addressesModelList.add(new AddressesModel(task.getResult().get("fullname_" + x).toString(),
+                                    task.getResult().get("address_" + x).toString(),
+                                    task.getResult().get("pincode_" + x).toString(),
+                                    (boolean) task.getResult().get("selected_" + x)));
+                            if ((boolean) task.getResult().get("selected_" + x)) {
+                                selectedAddress = Integer.parseInt(String.valueOf(x - 1));
+                            }
+                        }
 
                         deliveryIntent = new Intent(context, DeliveryActivity.class);
                     }
